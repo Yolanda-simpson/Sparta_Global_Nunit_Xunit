068ef6aee8aa16c17a202dc6cf47154701b4aecd
yolanda <ysimpson@spartaglobal.com>
2019-02-19 17:06:10 +0000

    test file added

---
+++ Nunit_Calculator/Nunit_Calculator/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nunit_Calculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
